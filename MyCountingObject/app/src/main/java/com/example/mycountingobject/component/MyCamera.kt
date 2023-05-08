a91c3a0855fc01e13d049bd9de14d9897b116913
13519016 <13519016@std.stei.itb.ac.id>
2023-05-08 14:32:52 +0700

    create request for camera permission

--- MyCountingObject/app/src/main/java/com/example/mycountingobject/component/MyCamera.kt
+++ MyCountingObject/app/src/main/java/com/example/mycountingobject/component/MyCamera.kt
@@ -1,5 +1,10 @@
 package com.example.mycountingobject.component
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.camera.core.CameraSelector
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
@@ -10,40 +15,57 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.camera.core.Preview
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.PermissionStatus
+import com.google.accompanist.permissions.rememberPermissionState
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun MyCamera(
     modifier: Modifier = Modifier
 ) {
     val lifecycleOwner = LocalLifecycleOwner.current
     val context = LocalContext.current
-    val cameraProviderFuture = remember { ProcessCameraProvider.getInstance(context) }
+    val permission = Manifest.permission.CAMERA
 
-    AndroidView(
-        factory = { ctx ->
-            val previewView = PreviewView(ctx)
-            val executor = ContextCompat.getMainExecutor(ctx)
-            cameraProviderFuture.addListener({
-                val cameraProvider = cameraProviderFuture.get()
-                val preview = Preview.Builder().build().also {
-                    it.setSurfaceProvider(previewView.surfaceProvider)
-                }
+    val permissionState = rememberPermissionState(permission)
+    if (permissionState.status is PermissionStatus.Denied){
+        Button(onClick = {permissionState.launchPermissionRequest()})
+        {
+            Text(text = "Request Camera Permission")  
+        }
+    }else{
+        val cameraProviderFuture = remember { ProcessCameraProvider.getInstance(context) }
 
-                val cameraSelector = CameraSelector.Builder()
-                    .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-                    .build()
+        AndroidView(
+            factory = { ctx ->
+                val previewView = PreviewView(ctx)
+                val executor = ContextCompat.getMainExecutor(ctx)
+                cameraProviderFuture.addListener({
+                    val cameraProvider = cameraProviderFuture.get()
+                    val preview = Preview.Builder().build().also {
+                        it.setSurfaceProvider(previewView.surfaceProvider)
+                    }
 
-                cameraProvider.unbindAll()
-                cameraProvider.bindToLifecycle(
-                    lifecycleOwner,
-                    cameraSelector,
-                    preview
-                )
-            }, executor)
-            previewView
-        },
-        modifier = modifier.fillMaxSize(),
-    )
+                    val cameraSelector = CameraSelector.Builder()
+                        .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                        .build()
+
+                    cameraProvider.unbindAll()
+                    cameraProvider.bindToLifecycle(
+                        lifecycleOwner,
+                        cameraSelector,
+                        preview
+                    )
+                }, executor)
+                previewView
+            },
+            modifier = modifier.fillMaxSize(),
+        )
+    }
+
 }
