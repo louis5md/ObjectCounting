a86e6190a478aa35a721842b482a610688f6e566
13519016 <13519016@std.stei.itb.ac.id>
2023-05-09 13:53:24 +0700

    Divide screen: Counting screen for counting object with camera and overview screen for seeing overview and add overview screen with fake data

--- MyCountingObject/app/src/main/java/com/example/mycountingobject/CountingObjectApp.kt
+++ MyCountingObject/app/src/main/java/com/example/mycountingobject/CountingObjectApp.kt
@@ -1,64 +1,18 @@
 package com.example.mycountingobject
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringArrayResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.mycountingobject.component.MyCamera
-import com.example.mycountingobject.component.MyDropDownMenu
-import com.example.mycountingobject.component.MyOneLineRadioButton
+import com.example.mycountingobject.ui.screen.CountingScreen
+import com.example.mycountingobject.ui.screen.OverviewScreen
 import com.example.mycountingobject.ui.theme.MyCountingObjectTheme
 
 @Composable
 fun CountingObjectApp(
     modifier: Modifier = Modifier,
 ) {
-    var isExpanded by remember { mutableStateOf(false) }
-    var isCounting by remember { mutableStateOf(false) }
-    var objectCounted by remember { mutableStateOf(0) }
-    var optionSelected by remember { mutableStateOf("") }
-
-    // Create a list of cities
-    val listMachine = stringArrayResource(id = R.array.list_machine).toList()
-
-    // Create a string value to store the selected city
-    var mSelectedText by remember { mutableStateOf("") }
-    Column(
-        modifier = Modifier
-            .wrapContentHeight(Alignment.Top),
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        MyDropDownMenu(
-            isExpanded = isExpanded,
-            listItem = listMachine,
-            onDismissRequest = {isExpanded = false },
-            onClick = {
-                mSelectedText = it
-                isExpanded = false
-            },
-            onValueChange = {mSelectedText = it},
-            text = mSelectedText,
-            onIconClick = {isExpanded = !isExpanded},
-            modifier = modifier,
-        )
-        MyOneLineRadioButton(
-            listOptions = listOf("IN","OUT","REJECT"),
-            optionSelected = optionSelected,
-            onClick = {optionSelected = it},)
-        Button(
-            onClick = { isCounting = !isCounting },
-        ) {
-            Text(text = if(!isCounting) "Start Counting" else "Stop Counting")
-        }
-        Text(text = stringResource(id = R.string.button_counting, objectCounted))
-        MyCamera(modifier = Modifier.padding(20.dp))
-    }
+    //CountingScreen()
+    OverviewScreen()
 }
 
 @Preview(
---
+++ MyCountingObject/app/src/main/java/com/example/mycountingobject/model/Machine.kt
@@ -0,0 +1,8 @@
+package com.example.mycountingobject.model
+
+data class Machine(
+    val name: String,
+    var IN: Int,
+    var OUT: Int,
+    var REJECT: Int,
+)
